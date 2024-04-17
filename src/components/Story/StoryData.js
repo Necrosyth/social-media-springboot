7a6cd777d5c755e95564b8b2f2748bb4df771c70
necro <acoustify69420@gmail.com>
2024-04-17 18:27:48 +0530

    initial

---
+++ src/components/Story/StoryData.js
@@ -0,0 +1,32 @@
+export const story = [
+    {
+      image:
+        "https://thispersondoesnotexist.com/",
+      username: "TestUser1",
+      userId: 1,
+    },
+    {
+      image:
+        "https://picsum.photos/400/500",
+      username: "Ramu",
+      userId: 1,
+    },
+    {
+      image:
+        "https://picsum.photos/300/400",
+      username: "George",
+      userId: 1,
+    },
+    {
+      image:
+        "https://picsum.photos/100/200",
+      username: "Paul",
+      userId: 4,
+    },
+    {
+      image:
+        "https://picsum.photos/200/300",
+      username: "John",
+      userId: 5,
+    },
+  ];
